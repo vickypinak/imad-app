82b09170766d21960572a86b6bc1d788c6f0025a
vickypinak <pinakbagchi115@gmail.com>
2018-03-02 03:32:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,13 @@
 console.log('Loaded!');
 alert('PageLoaded!');
+
+var img = document.getElementById('img_madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft+1;
+    img.style.marginLeft = marginLeft + 5;
+}
+img.onclick = function ()
+{
+  var interval = setInterval(moveRight,50);  
+};
