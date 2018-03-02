b24f0d3b8df7b9e69f4e8e67ded72260e9a795f9
vickypinak <pinakbagchi115@gmail.com>
2018-03-02 12:17:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,19 +13,24 @@ img.onclick = function ()
 
 //onButtonClickCountIncrease
 
-var request = XMLHttpRequest();
+var button = document.getElementById('counter');
 
-request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE)
-     {
-         if(request.status===200)
+    button.onclick = function(){
+        
+        var request = XMLHttpRequest();
+        
+        request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE)
             {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+             if(request.status===200)
+                {
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                }
             }
-     }
+        };
+        
+        request.open('GET','http://pinakbagchi115.imad.hasura-app.io/counte',true);
+        request.send(null);
 };
-
-request.open('GET','http://pinakbagchi115.imad.hasura-app.io/counter',true);
-request.send(null);
