75e1f5602812bad2a27182d86c713098926adf2d
vickypinak <pinakbagchi115@gmail.com>
2018-03-02 11:47:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,11 @@ app.get('/:articleName', function(req,res){
     res.send(createTemplate(contents[articleNames]));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
