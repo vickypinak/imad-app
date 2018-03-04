7e5d394695ce9e2f76b37b06424eb2fa35bc653d
vickypinak <pinakbagchi115@gmail.com>
2018-03-05 00:10:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/db-test',function(req,res){
             res.status(500).send(err.toString());
         } 
         else {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });    
 });
