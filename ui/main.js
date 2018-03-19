f4b43b802702b8ba0bea4908ed7c92434d20f1f9
vickypinak <pinakbagchi115@gmail.com>
2018-03-19 22:48:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,6 +37,18 @@ var button = document.getElementById('counter');
 
 // Searchbox
 
+var submit_search = document.getElementById('submit-name');
+submit_search.onClick = function(){
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0;i<names.length;i++) {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var submit_list = document.getElementById('name-list');
+    submit_list.innerHTML = list;
+};
+
+
  
 
 
