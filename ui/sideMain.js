a703c18d0d8ef982ff54ff71ffb66abdc4918e6c
vickypinak <pinakbagchi115@gmail.com>
2018-03-19 21:44:05 +0530

    [imad-console] Updates ui/sideMain.js

--- ui/sideMain.js
+++ ui/sideMain.js
@@ -1,1 +1,11 @@
-
+var button = document.getElementById('nameSubmit');
+var names = ['name1','name2','name3','name4','name5'];
+var list = '';
+button.onClick = function() {
+ for(var i = 0;i<names.lenght;i++)
+ {
+     list+= '<li' + names[i] + '</li>';
+ }
+ var returnList = document.getElementById('list_id');                     //Returns the list to the ui in search.html
+ returnList.innerHTML = list;
+};
