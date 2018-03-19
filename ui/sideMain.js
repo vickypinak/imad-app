74afac51b7515330f4f5b9bf1c79a55d346f9482
vickypinak <pinakbagchi115@gmail.com>
2018-03-19 21:52:13 +0530

    [imad-console] Updates ui/sideMain.js

--- ui/sideMain.js
+++ ui/sideMain.js
@@ -2,10 +2,10 @@ var button = document.getElementById('nameSubmit');
 var names = ['name1','name2','name3','name4','name5'];
 var list = '';
 button.onClick = function() {
- for(var i = 0;i<names.lenght;i++)
+ for(var i = 0;i<6;i++)
  {
-     list+= '<li' + names[i] + '</li>';
-     console.log(JSON.stringfy(list));
+    // list+= '<li' + names[i] + '</li>';
+    // console.log(JSON.stringfy(list));
      console.log('1');
  }
  var returnList = document.getElementById('list_id');                     //Returns the list to the ui in search.html
